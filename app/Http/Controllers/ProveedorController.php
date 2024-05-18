fc07636710d5b85df05b9aa2842a51434e8b577f
josemanuelct10 <114997961+josemanuelct10@users.noreply.github.com.>
2024-05-18 17:05:09 +0200

    Validaciones

--- app/Http/Controllers/ProveedorController.php
+++ app/Http/Controllers/ProveedorController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Proveedor;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
 use Symfony\Component\HttpFoundation\Response;
 use App\Http\Requests\ProveedorRequest; // Importa ProveedorRequest desde App\Http\Requests
@@ -21,22 +22,37 @@ class ProveedorController extends Controller
 
     /**
      * Método que crea un nuevo proveedor
-     * Recibe un ProveedorRequest para validar y crear el proveedor
+     * Recibe un Request  para crear el proveedor
      * Devuelve una respuesta JSON
      */
-    public function store(ProveedorRequest $request): JsonResponse
+    public function store(Request $request): JsonResponse
     {
-        try {
-            $proveedor = Proveedor::create($request->validated());
+        $cif = $request->input('cif');
+
+        // Verificar si ya existe un proveedor con el mismo CIF
+        $existingProveedor = Proveedor::where('cif', $cif)->first();
+        if ($existingProveedor) {
             return response()->json([
+                'response' => -1,
+                'success' => false,
+                'message' => 'Ya existe un proveedor con este CIF.'
+            ], 201);
+        }
+
+        try {
+            // Crear el proveedor si no existe
+            $proveedor = Proveedor::create($request->all());
+            return response()->json([
+                'response' => 1,
                 'success' => true,
                 'data' => $proveedor
             ], Response::HTTP_CREATED);
         } catch (QueryException $exception) {
             return response()->json([
+                'response' => 0,
                 'success' => false,
                 'message' => "Error al crear el proveedor. Por favor, inténtalo de nuevo."
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+            ], Response:: HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -59,7 +75,8 @@ class ProveedorController extends Controller
             $proveedor->delete();
 
             return response()->json([
-                'success' => true
+                'success' => true,
+                'response' => 1
             ], 200);
         } catch (ModelNotFoundException $e) {
             return response()->json([
@@ -69,7 +86,7 @@ class ProveedorController extends Controller
         } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
-                'error' => 'Se produjo un error al intentar eliminar el proveedorº.'
+                'error' => 'Se produjo un error al intentar eliminar el proveedor.'
             ], 500);
         }
     }
@@ -82,6 +99,7 @@ class ProveedorController extends Controller
 
             if (!$proveedor) {
                 return response()->json([
+                    'response' => -1,
                     'success' => false,
                     'message' => 'Proveedor no encontrado',
                 ], 404);
@@ -95,11 +113,13 @@ class ProveedorController extends Controller
             $proveedor->save();
 
             return response()->json([
+                'response' => 1,
                 'success' => true,
                 'data' => $proveedor,
             ], 200);
         } catch (\Exception $e) {
             return response()->json([
+                'response' => 0,
                 'success' => false,
                 'message' => 'Error al actualizar el proveedor: ' . $e->getMessage(),
             ], 500);
